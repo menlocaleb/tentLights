5a27c1c37258efea1381d55df0873a8272f5561a
Caleb Young <menlocaleb@gmail.com>
2014-06-18 00:49:40 -0700

    have a cube shaped room with box inside it and one spot light - ready to start developing actual interface

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ function main() {
 }
 
 
+
+function addLight(scene) {
+	var spotLight = new THREE.SpotLight( 0xffffff );
+	spotLight.position.set( 0, 4, 9 );
+	var target = new THREE.Vector3(0,1,1);
+	target.negate().add(spotLight.position);
+	spotLight.target.position = target;
+	console.log(target);
+
+	spotLight.castShadow = true;
+
+	spotLight.shadowMapWidth = 1024;
+	spotLight.shadowMapHeight = 1024;
+
+	spotLight.shadowCameraNear = 500;
+	spotLight.shadowCameraFar = 4000;
+	spotLight.shadowCameraFov = 30;
+
+	scene.add( spotLight );
+}
+
+
 // Until have model of DM tent, use this to test lights inside of
 // North is + y, East is + x
 function createRoom(scene) {
@@ -16,38 +38,78 @@ function createRoom(scene) {
 		tentHeight = 10;
 
 	// need phong b/c I think lambert is evaluated per vertex which for plane doesn't work
-	var floorMaterial = new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0x222222, specular: 0xcccccc, shininess: 30, shading: THREE.FlatShading } );
+	var floorMaterial = new THREE.MeshPhongMaterial( { ambient: 0x222222, color: 0x222222, specular: 0xcccccc, shininess: 50, shading: THREE.FlatShading } );
 	//floorMaterial.side = THREE.DoubleSide;
-	var wallMaterial = new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xeeeeee, specular: 0xffffff, shininess: 30, shading: THREE.FlatShading } );
+	var wallMaterial = new THREE.MeshPhongMaterial( { ambient: 0xeeeeee, color: 0xeeeeee, specular: 0xcccccc, shininess: 50, shading: THREE.FlatShading } );
 	//wallMaterial.side = THREE.DoubleSide;
 
 	// create floor of room
-	var tentFloor = new THREE.Geometry();
-	tentFloor.vertices.push( new THREE.Vector3(tentWidth/2, tentLength/2, 0));
-	tentFloor.vertices.push( new THREE.Vector3(-tentWidth/2, tentLength/2, 0));
-	tentFloor.vertices.push( new THREE.Vector3(-tentWidth/2, -tentLength/2, 0));
-	tentFloor.vertices.push( new THREE.Vector3(tentWidth/2, -tentLength/2, 0));
-
-	tentFloor.faces.push( new THREE.Face3( 0, 1, 2, new THREE.Vector3(0,0,1)) );
-	tentFloor.faces.push( new THREE.Face3( 2, 3, 0, new THREE.Vector3(0,0,1) ) );
+	var tentFloor = new THREE.PlaneGeometry( tentWidth, tentLength );
 
 	var floorMesh = new THREE.Mesh( tentFloor, floorMaterial ) ;
 	floorMesh.receiveShadow = true;
 
-	// create  of room
-	/*var tentSouthWall = new THREE.Geometry();
-	tentSouthWall.vertices.push( new THREE.Vector3(-tentWidth/2, -tentLength/2, 0));
-	tentSouthWall.vertices.push( new THREE.Vector3(-tentWidth/2, -tentLength/2, tentHeight));
-	tentSouthWall.vertices.push( new THREE.Vector3(tentWidth/2, -tentLength/2, tentHeight));
-	tentSouthWall.vertices.push( new THREE.Vector3(tentWidth/2, -tentLength/2, 0));
-
-	tentSouthWall.faces.push( new THREE.Face3( 0, 1, 2 ) );
-	tentSouthWall.faces.push( new THREE.Face3( 2, 3, 0 ) );
-
-	var southWallMesh = new THREE.Mesh( tentSouthWall, wallMaterial ) ;	*/	
-
 	scene.add( floorMesh );
-	//scene.add( southWallMesh );
+
+
+
+	// create south wall of room
+	var tentSouthWall = new THREE.PlaneGeometry( tentWidth, tentHeight );
+
+	var southWallMesh = new THREE.Mesh( tentSouthWall, wallMaterial ) ;
+	southWallMesh.position.y = -tentLength/2;
+	southWallMesh.position.z = tentHeight/2;
+	southWallMesh.rotation.x = -Math.PI/2;
+
+	scene.add( southWallMesh );
+
+
+	// create north wall of room
+	var tentNorthWall = new THREE.PlaneGeometry( tentWidth, tentHeight );
+
+	var northWallMesh = new THREE.Mesh( tentNorthWall, wallMaterial ) ;
+	northWallMesh.position.y = tentLength/2;
+	northWallMesh.position.z = tentHeight/2;
+	northWallMesh.rotation.x = Math.PI/2;
+
+	scene.add( northWallMesh );
+
+
+	// create west wall of room
+	var tentWestWall = new THREE.PlaneGeometry( tentHeight, tentWidth );
+
+	var westWallMesh = new THREE.Mesh( tentWestWall, wallMaterial ) ;
+	westWallMesh.position.x = -tentWidth/2;
+	westWallMesh.position.z = tentHeight/2;
+	westWallMesh.rotation.y = Math.PI/2;
+
+	scene.add( westWallMesh );
+
+
+	// create east wall of room
+	var tentEastWall = new THREE.PlaneGeometry( tentHeight, tentWidth );
+
+	var eastWallMesh = new THREE.Mesh( tentEastWall, wallMaterial ) ;
+	eastWallMesh.position.x = tentWidth/2;
+	eastWallMesh.position.z = tentHeight/2;
+	eastWallMesh.rotation.y = -Math.PI/2;
+
+	scene.add( eastWallMesh );
+
+
+
+	// create roof of room
+	var tentCeiling = new THREE.PlaneGeometry( tentWidth, tentHeight );
+
+	var ceilingMesh = new THREE.Mesh( tentCeiling, wallMaterial ) ;
+	ceilingMesh.position.z = tentHeight;
+	ceilingMesh.rotation.x = Math.PI;
+
+	scene.add( ceilingMesh );
+
+
+	
+	
 }
 
 
